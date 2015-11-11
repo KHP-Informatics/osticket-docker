aa4a68ea44cef71ef86353733036c060ac6f507f
Cass Johnston <cassjohnston@gmail.com>
2015-11-11 13:41:25 +1300

    doc change

--- install_osticket.sh
+++ install_osticket.sh
@@ -6,5 +6,6 @@ unzip -d osticket osTicket-v1.8.12.zip
 mv osticket/upload /usr/local/apache2/htdocs/osticket
 cp /usr/local/apache2/htdocs/osticket/include/ost-sampleconfig.php /usr/local/apache2/htdocs/osticket/include/ost-config.php
 chown -R osticket:osticket /usr/local/apache2
+
 rm -rf /tmp/*
 
