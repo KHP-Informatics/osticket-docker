79e43048aced60675ed674468e3d9498bc4f115b
Cass Johnston <cassjohnston@gmail.com>
2016-11-02 09:03:53 +1300

    first attempt at 1.10

--- install_osticket.sh
+++ install_osticket.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 cd /tmp
-wget "http://osticket.com/sites/default/files/download/osTicket-v1.10-rc.3.zip"
-unzip osTicket-v1.10-rc.3.zip
+wget "https://github.com/osTicket/osTicket/releases/download/v1.10/osTicket-v1.10.zip"
+unzip osTicket-v1.10.zip
 mv upload /usr/local/apache2/htdocs/osticket
 cp /usr/local/apache2/htdocs/osticket/include/ost-sampleconfig.php /usr/local/apache2/htdocs/osticket/include/ost-config.php
 chown -R osticket:osticket /usr/local/apache2
